dcfd1c3e92295418dc7a4fcc2a0da1c58f938541
Chandrakant Shinde <chandrakant.shinde@myridz.com>
2017-02-24 10:18:39 +0530

    Minor bug fixes

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -10,7 +10,7 @@ class ChatroomsController < ApplicationController
   # GET /chatrooms/1
   # GET /chatrooms/1.json
   def show
-    @chatroom = Chatroom.find_by(slug: params[:slug])
+    @chatroom = Chatroom.find_by(id: params[:id])
     @message = Message.new
   end
   # GET /chatrooms/new
